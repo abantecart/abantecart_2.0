48d63fa60692dea96396741c443e9deff5e8d40e
abolabo <abolabo@gmail.com>
2023-08-30 13:26:52 +0300

    core: nullableInt castable minor fix

--- abantecart/abc/models/casts/NullableInt.php
+++ abantecart/abc/models/casts/NullableInt.php
@@ -1,19 +1,19 @@
 <?php
 /**
  * AbanteCart, Ideal Open Source Ecommerce Solution
- * http://www.abantecart.com
+ * https://www.abantecart.com
  *
- * Copyright 2011-2022 Belavier Commerce LLC
+ * Copyright (c) 2011-2023  Belavier Commerce LLC
  *
  * This source file is subject to Open Software License (OSL 3.0)
  * License details is bundled with this package in the file LICENSE.txt.
  * It is also available at this URL:
- * <http://www.opensource.org/licenses/OSL-3.0>
+ * <https://www.opensource.org/licenses/OSL-3.0>
  *
  * UPGRADE NOTE:
  * Do not edit or add to this file if you wish to upgrade AbanteCart to newer
  * versions in the future. If you wish to customize AbanteCart for your
- * needs please refer to http://www.abantecart.com for more information.
+ * needs please refer to https://www.abantecart.com for more information.
  */
 
 namespace abc\models\casts;
@@ -31,11 +31,11 @@ class NullableInt implements CastsAttributes
      * @param  string  $key
      * @param  mixed  $value
      * @param  array  $attributes
-     * @return array
+     * @return int|null
      */
     public function get($model, $key, $value, $attributes)
     {
-        return $value;
+        return (int)$value ?: null;
     }
 
     /**
@@ -51,5 +51,4 @@ class NullableInt implements CastsAttributes
     {
         return (int)$value ?: null;
     }
-
 }
